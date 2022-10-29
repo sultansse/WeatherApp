6f426f5e6972cafbd71f3af9880ff3758970f035
BexultanN <beksultan.nurakyn@gmail.com>
2022-10-30 00:44:57 +0600

    NavBar added MainActivity.kt

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +13,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemReselectedListener {
+
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.news -> replaceFragment(NewsFragment())
+                R.id.search -> replaceFragment(SearchFragment())
+                R.id.about -> replaceFragment(AboutFragment())
+
+                else ->{
+
+                }
+            }
+
+            true
+
+        }
 
     }
 
+    private fun replaceFragment(fragment: Fragment){
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.frame_layout, fragment)
+            fragmentTransaction.commit()
+    }
     
 }
