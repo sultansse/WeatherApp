d0cc05feaf54759677dd6ec146a8441ae24fd848
Sultan Suleimenov <ss.softwareit@gmail.com>
2022-11-02 19:37:51 +0600

    not working code, just saving code to not lose it set in navigation home page as default added recyclerview xml to homepage

--- app/src/main/java/com/example/weatherapp/HomeFragment.kt
+++ app/src/main/java/com/example/weatherapp/HomeFragment.kt
@@ -1,10 +1,13 @@
 package com.example.weatherapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weatherapp.homePageElements.recyclerToday.Datasource
+import com.example.weatherapp.homePageElements.recyclerToday.ItemAdapter
 
 class HomeFragment : Fragment() {
 
@@ -12,8 +15,22 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+//        val myDataset = Datasource().loadAffirmations()
+//        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+//        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+//        recyclerView.setHasFixedSize(true)
+    }
+
 
 }
---
+++ app/src/main/java/com/example/weatherapp/homePageElements/recyclerToday/Affirmation.kt
@@ -0,0 +1,9 @@
+package com.example.weatherapp.homePageElements.recyclerToday
+
+data class Affirmation(
+    val tempOfTime: Int,
+    val weatherIconPerHour: Int,
+    val timeForToday: Int
+)
+
+
