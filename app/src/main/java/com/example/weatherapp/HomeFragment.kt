f72ce1f93ba5a7d907e41df7b627ff9ce19ffc85
BexultanN <beksultan.nurakyn@gmail.com>
2022-11-03 01:46:43 +0600

    Merge remote-tracking branch everything is word
    # Conflicts:
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/weatherapp/HomeFragment.kt
+++ app/src/main/java/com/example/weatherapp/HomeFragment.kt
@@ -1,10 +1,16 @@
 package com.example.weatherapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ScrollView
+import androidx.core.view.ViewCompat.isNestedScrollingEnabled
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weatherapp.homePageElements.recyclers.Datasource
+import com.example.weatherapp.homePageElements.recyclers.TodayItemAdapter
+import com.example.weatherapp.homePageElements.recyclers.WeekItemAdapter
 
 class HomeFragment : Fragment() {
 
@@ -14,6 +20,22 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val todayDataset = Datasource().loadTodayViews()
+        val todayRecyclerview = getView()?.findViewById<RecyclerView>(R.id.todayDetailsRecycler)
+        todayRecyclerview?.adapter = TodayItemAdapter(this, todayDataset)
+        todayRecyclerview?.setHasFixedSize(true)
+
+        val weekDataset = Datasource().loadWeekViews()
+        val weekRecyclerview = getView()?.findViewById<RecyclerView>(R.id.weekDaysDetailsRecycler)
+        weekRecyclerview?.adapter = WeekItemAdapter(this, weekDataset)
+        weekRecyclerview?.setHasFixedSize(true)
+
+
     }
 
 }
